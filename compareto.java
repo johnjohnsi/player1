03da1e14d3324c324748658b239768ea40baf5df
johnjohnsi <johnsiranimadesh@gmail.com>
2017-06-06 14:56:50 +0530

    Add files via upload

---
+++ compareto.java
@@ -0,0 +1,17 @@
+package john;
+
+public class compareto {
+
+	public static void main(String[] args) {
+		String s1="johnsirani";
+		String s2="john";
+		String s3="john";
+		String s4="john";
+		System.out.println(s1.contains(s2));
+		System.out.println(s2.contains(s3));
+		System.out.println(s3.contains(s4));
+		System.out.println(s4.contains(s1));
+		
+	}
+
+}
