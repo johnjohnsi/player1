240beda8029384882391dee8d4e4a181ffd91753
johnjohnsi <johnsiranimadesh@gmail.com>
2017-06-23 09:32:43 +0530

    Add files via upload

---
+++ sumofn.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class sumofn {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter teh number");
+		int num=s.nextInt();
+		int n=0;
+		for(int i=0;i<=num;i++){
+			n+=i;
+		}
+		System.out.println("sum of n : "+n);
+		s.close();
+	}
+
+}
