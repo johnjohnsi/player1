348f7541ae19212f369041819ccdecc018df889b
johnjohnsi <johnsiranimadesh@gmail.com>
2017-06-30 14:52:11 +0530

    Add files via upload

---
+++ Fib.java
@@ -0,0 +1,25 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Fib {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=s.nextInt();
+		int n1=0;
+		int n2=1;
+		int temp;
+		System.out.println(n1+"\n"+n2);
+		for(int i=2;i<num;i++){
+			temp=n1+n2;
+			System.out.println(temp);
+			n1=n2;
+			n2=temp;
+		}
+		s.close();
+
+	}
+
+}
