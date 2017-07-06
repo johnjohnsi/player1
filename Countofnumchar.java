317abab58541ecab08d738f9ca59c73f65211352
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-06 14:23:27 +0530

    Add files via upload

--- Countofnumchar.java
+++ Countofnumchar.java
@@ -6,12 +6,14 @@ public class Countofnumchar {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the string : ");
 		String str=s.nextLine();
-		System.out.println("The numerical character in the given string : ");
+		int count=0;
+		System.out.println("The numerical character count in the given string : ");
 		for(int i=0;i<str.length();i++){
 		if(str.charAt(i)>='0' && str.charAt(i)<='9'){
-			System.out.print(str.charAt(i)+" ");
+			count++;
 		}
 		}
+		System.out.println(count);
 		s.close();
 	}
 }
