8ddea4de922fc307fb7cbde2877735fbbcb57ff4
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-13 09:08:50 +0530

    Add files via upload

---
+++ Collect.java
@@ -0,0 +1,21 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Collect{
+	public static void main(String[] main){
+		ArrayList<Integer> num=new ArrayList<Integer>();
+		ArrayList<String> str=new ArrayList<String>();
+		ArrayList<String> addr=new ArrayList<String>();
+		ArrayList<Float> sal=new ArrayList<Float>();
+		num.add(01);
+		str.add("john");
+		addr.add("Harur");
+		sal.add((float) 1000.5);
+		System.out.println(num);
+		System.out.println(str);
+		System.out.println(addr);
+		System.out.println(sal);
+		
+	}
+}
