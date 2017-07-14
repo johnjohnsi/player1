70707de2a17dabcc0ff6fcd7026ecc0636d3bbfe
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-14 15:35:07 +0530

    Add files via upload

---
+++ JustpaySquareroot.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class JustpaySquareroot {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println( "Enter X1 : " );
+		int x1 = s.nextInt();
+		System.out.println( "Enter X2 : " );
+		int x2 = s.nextInt();
+		System.out.println( "Enter Y1 : " );
+		int y1 = s.nextInt();
+		System.out.println( "Enter Y2 : " );
+		int y2 = s.nextInt();
+		int num =  ( x1 + x2 ) * ( x1 + x2 ) + ( y1 + y2 ) * ( y1 + y2 );
+		System.out.println( num );
+		s.close();
+	}
+}
