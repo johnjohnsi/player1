f7afecccc67d21dafc00cebeef592074351a6169
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-15 15:47:22 +0530

    Add files via upload

---
+++ MultiTable.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class MultiTable {
+	public static void main(String[] args) {
+		int num,i,mul;
+		Scanner n=new Scanner(System.in);
+		System.out.println("Enter the number");
+		num=n.nextInt();
+		for(i=1;i<10;i++)
+		{
+			 mul=i*num;
+			System.out.println(i+"*"+num+"="+mul);
+		}
+		n.close();
+	}
+}
