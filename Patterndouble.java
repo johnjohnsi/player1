81382ee5eb75a223b7e4e6dec07c03ac7f362125
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-15 14:27:49 +0530

    Add files via upload

---
+++ Patterndouble.java
@@ -0,0 +1,20 @@
+package guvi;
+import java.util.*;
+public class Patterndouble {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println( "Enter the Input1 : " );
+		int num = s.nextInt();
+		for( int i = num ; i>=0 ; i-- ) {
+			for( int j = 1 ; j<=i ; j++ ) {
+				System.out.print( "*" );
+			}
+			System.out.print(" ");
+			for( int j = 1 ; j<=i ; j++ ) {
+				System.out.print( "*" );
+			}
+			System.out.println();
+		}
+		s.close();
+	}
+}
