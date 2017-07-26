0bbe3f0d372b7de7250ab4d8e5c8aadd34c63df4
johnjohnsi <johnsiranimadesh@gmail.com>
2017-07-26 14:54:06 +0530

    Add files via upload

---
+++ PowerNearPower.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.*;
+public class PowerNearPower {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println( "Enter the Input : " );
+		int input = s.nextInt();
+		int rem = 0 ;
+	
+		while( input != 0 ) {
+			rem = input % 10;
+			sum = rem;
+		}
+	}
+}
