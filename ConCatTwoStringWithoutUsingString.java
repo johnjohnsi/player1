570b27b1297da22507ba81daa43aadf1f05045ad
johnjohnsi <johnsiranimadesh@gmail.com>
2017-08-19 17:16:21 +0530

    Add files via upload

---
+++ ConCatTwoStringWithoutUsingString.java
@@ -0,0 +1,11 @@
+package guvi;
+import java.util.*;
+public class ConCatTwoStringWithoutUsingString {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input1 = s.next();
+		String input2 = s.next();
+		System.out.println(input1+input2);
+		s.close();
+	}
+}
